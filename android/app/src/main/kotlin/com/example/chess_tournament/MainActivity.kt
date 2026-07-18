04c0c9749efbd644a8d8f194378a7478760dafa0
shivanihashilkar <shivanihashilkar@gmail.com>
2026-07-18 11:07:58 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/chess_tournament/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.chess_tournament
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Snehal hashilkar\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Snehal hashilkar\Downloads\chess_tournament_sp"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\Users\Snehal hashilkar\Downloads\chess_tournament_sp\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
